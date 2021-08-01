cd94c4cca0d4b85f69b2ad71c7f215bf235478f7
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-01 23:30:15 +0530

    Update NoteCard.js

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,1 +1,21 @@
 
+ <List>
+                    {menuItems.map(item => (
+                        <ListItem
+                            button
+                        key={item.text}
+                        onClick={() => history.push(item.path)}
+                        className = {location.pathname === item.path ? classes.active : null}>
+
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text}/>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <div className={classes.page}>
+                <div className={classes.toolbar}></div>
+                {props.children}
+
+            </div>
+        </div>
