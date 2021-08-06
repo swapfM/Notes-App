4591227e840fccb079e241ff9c9133e21d24ba3d
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-06 23:25:54 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -101,38 +101,23 @@ function Layout (props){
             anchor={"left"}
             classes = {{paper: classes.drawerPaper}}
             >
-                
+
                 <div>
                     <Typography variant={"h5"} className={classes.title}>
                        SwapfM's Notes
                     </Typography>
                 </div>
 
-                <List>
-                    {menuItems.map(item => (
-                        <ListItem
-                            button
-                        key={item.text}
-                        onClick={() => history.push(item.path)}
-                        className = {location.pathname === item.path ? classes.active : null}>
-
-                            <ListItemIcon>{item.icon}</ListItemIcon>
-                            <ListItemText primary={item.text}/>
-                        </ListItem>
-                    ))}
-                </List>
+               
             </Drawer>
-                    
-                 <div className={classes.page}>
+            <div className={classes.page}>
                 <div className={classes.toolbar}></div>
                 {props.children}
 
-           
             </div>
         </div>
 
     )
-
 
 }
 
