d42c58661014be530bcbc2e183e69d65afe784c2
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-06 23:28:43 +0530

    Update Notes.js

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,1 +1,12 @@
 
+import React, {useEffect, useState} from 'react'
+import {Card, Container, Grid, Paper} from "@material-ui/core";
+import NoteCard from "../components/NoteCard";
+import Masonry from "react-masonry-css"
+
+export default function Notes() {
+
+    const [notes, setNotes] = useState([])
+
+
+  
