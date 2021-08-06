5d80eb257f95f7ea8c135f61b5085d36ca1a621e
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-06 23:26:05 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -107,7 +107,19 @@ function Layout (props){
                        SwapfM's Notes
                     </Typography>
                 </div>
+                 <List>
+                    {menuItems.map(item => (
+                        <ListItem
+                            button
+                        key={item.text}
+                        onClick={() => history.push(item.path)}
+                        className = {location.pathname === item.path ? classes.active : null}>
 
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text}/>
+                        </ListItem>
+                    ))}
+                </List>
                
             </Drawer>
             <div className={classes.page}>
