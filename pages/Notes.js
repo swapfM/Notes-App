91474b259946d13396fa240589fd5c44c4509ac4
swapfM <58875865+swapfM@users.noreply.github.com>
2021-08-11 22:37:48 +0530

    Update Notes.js

--- pages/Notes.js
+++ pages/Notes.js
@@ -1,1 +1,15 @@
+import React, {useEffect, useState} from 'react'
+import {Card, Container, Grid, Paper} from "@material-ui/core";
+import NoteCard from "../components/NoteCard";
+import Masonry from "react-masonry-css"
+
+export default function Notes() {
+
+   
+    const breakpoints = {
+        default: 3,
+        1100: 2,
+        700: 1
+    }
+
 
